979a11a9f826553721dfe52668aa1591c9233ad0
anavic44 <a01567111@tec.mx>
2025-06-03 13:47:45 -0600

    first commit

---
+++ src/main/java/com/taylorsongs/songs/TaylorSongsApplication.java
@@ -0,0 +1,13 @@
+package com.taylorsongs.songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaylorSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaylorSongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taylorsongs/songs/TaylorSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taylorsongs.songs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaylorSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
